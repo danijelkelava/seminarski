6b5641b31d003782cf56c50e58dbfa2ed2665513
danijelkelava <dykyprod@gmail.com>
2018-03-18 14:27:58 +0100

    grid update

--- resources/views/zanr.blade.php
+++ resources/views/zanr.blade.php
@@ -5,23 +5,26 @@
 @section('heading', 'Zanrovi')
 
 @section('container')
-<div class="py-4">
-	@include('layouts.errors')
-	<form method="POST" action="/zanr">
-		@csrf
-	  <div class="form-group">
-	    <label for="zanr">Naslov zanra</label>
-	    <input type="text" class="form-control" id="zanr" name="naziv" aria-describedby="zanr" placeholder="Unesi naslov zanra">
-	  </div>
-	  <button type="submit" class="btn btn-primary">Unesi zanr</button>
-	</form>	
+<div class="row">
+	<div class="col-xs-12 col-sm-6 col-md-6 py-4">
+		@include('layouts.errors')
+		<form method="POST" action="/zanr">
+			@csrf
+		  <div class="form-group">
+		    <label for="zanr">Naslov zanra</label>
+		    <input type="text" class="form-control" id="zanr" name="naziv" aria-describedby="zanr" placeholder="Unesi naslov zanra">
+		  </div>
+		  <button type="submit" class="btn btn-primary">Unesi zanr</button>
+		</form>	
+	</div>
+	<div class="col-xs-12 col-sm-6 col-md-6 py-4">
+		<h2>Lista zanrova</h2>
+		<ul class="list-group">
+			@foreach($zanrs as $zanr)
+			<li class="list-group-item">{{ $zanr->naziv }}</li>
+			@endforeach
+		</ul>
+	</div>
 </div>
-<div class="py-4">
-	<h2>Lista zanrova</h2>
-	<ul class="list-group">
-		@foreach($zanrs as $zanr)
-		<li class="list-group-item">{{ $zanr->naziv }}</li>
-		@endforeach
-	</ul>
-</div>
+
 @endsection
