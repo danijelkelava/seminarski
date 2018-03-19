fcf9bfa6cef123f40ded07f2e646233a730790aa
danijelkelava <dykyprod@gmail.com>
2018-03-19 15:50:49 +0100

    drugi izbornik

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,10 @@
 		@foreach($collection as $letter)
 		<a class="btn btn-outline-primary" href="/filmovi/{{ $letter }}">{{ $letter }}</a>
 		@endforeach
+		<?php $letters = range('a', 'z') ?>
+		<?php foreach($letters as $let) : ?>
+			<a class="btn btn-outline-primary" href="/filmovi/<?php echo strtoupper($let); ?>" ><?php echo $let; ?></a>
+		<?php endforeach; ?>
 	</nav>
 </div>
 @endsection
