436de74ab5cc2d8957d3c13342b0ae7afc85ee22
danijelkelava <dykyprod@gmail.com>
2018-03-26 20:29:40 +0200

    filmovi templejt dk

--- resources/views/filmovi/index.blade.php
+++ resources/views/filmovi/index.blade.php
@@ -12,9 +12,9 @@
 	@foreach($films as $film)
 	<div class="card my-2 mx-2" style="width: 10rem;">
 	  <img class="card-img-top" src="{{ asset($film->slika) }}" alt="Card image cap">
-	  <div class="card-body">
-	    <h5 class="card-title">{{ $film->naslov }}({{ $film->godina }})</h5>
-	    <p class="card-text">Trajanje:&nbsp;{{ $film->trajanje }} min</p>
+	  <div class="card-body bg-light">
+	    <h5 class="card-title">{{ $film->naslov }} ({{ $film->godina }})</h5>
+	    <p class="card-text">Trajanje: {{ $film->trajanje }}&nbsp;min</p>
 	  </div>
 	</div>
 	@endforeach
